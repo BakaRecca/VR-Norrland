81a298d214e8f380d15e40a76f6d58807d771292
Eddie Norberg <eddie.norberg@gmail.com>
2021-09-06 15:59:35 +0200

    typo fixes.

--- Norrland/Assets/Scripts/HMDInfoManager.cs
+++ Norrland/Assets/Scripts/HMDInfoManager.cs
@@ -1,22 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class HMDInfoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Is Device Active " + XRSettings.isDeviceActive);
-        Debug.Log(" Device name is : " + XRSettings.loadedDeviceName);
+        Debug.Log("Device name is : " + XRSettings.loadedDeviceName);
 
         if (!XRSettings.isDeviceActive)
         {
-            Debug.Log("No headset Plugged");
+            Debug.Log("No headset plugged");
         }
-
-        else if (XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == " Mock HMD" || XRSettings.loadedDeviceName == "MockHMDDisplay"))
+        else if (XRSettings.isDeviceActive &&
+                 (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMDDisplay"))
         {
             Debug.Log("Using Mock HMD");
         }
@@ -24,11 +21,5 @@ public class HMDInfoManager : MonoBehaviour
         {
             Debug.Log("We have a headset" + XRSettings.loadedDeviceName);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
