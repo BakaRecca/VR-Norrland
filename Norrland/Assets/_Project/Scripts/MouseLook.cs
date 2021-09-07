f3a8a121a728eef7a44873e8bb73ff5bef63b0f3
Chris <70510078+cseehuusen@users.noreply.github.com>
2021-09-07 11:06:00 +0200

    Moved between folders

---
+++ Norrland/Assets/_Project/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    public InputActionReference horizontalLook;
+    public InputActionReference verticalLook;
+    public float lookSpeed = .11f;
+    public Transform cameraTransform;
+    float pitch;
+    float yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        horizontalLook.action.performed += HandleHorizontalLookChange;
+        verticalLook.action.performed += HandleVerticalLookChange;
+    }
+
+    void HandleHorizontalLookChange(InputAction.CallbackContext obj)
+    {
+        yaw += obj.ReadValue<float>();
+        transform.localRotation = Quaternion.AngleAxis(yaw * lookSpeed, Vector3.up);
+    }
+
+    void HandleVerticalLookChange(InputAction.CallbackContext obj)
+    {
+        pitch -= obj.ReadValue<float>();
+        cameraTransform.localRotation = Quaternion.AngleAxis(pitch * lookSpeed, Vector3.right);
+    }
+}
