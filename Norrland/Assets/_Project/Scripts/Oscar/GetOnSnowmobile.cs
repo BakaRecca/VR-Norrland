0eb76be89a405e4066e37a03056fb4d1560a0522
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2021-09-24 14:22:57 +0200

    Updated lake scene with teleport and began working on attaching the Player to snowmobile

---
+++ Norrland/Assets/_Project/Scripts/Oscar/GetOnSnowmobile.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class GetOnSnowmobile : MonoBehaviour
+{
+    private Interactable _interactable;
+
+    private void Awake()
+    {
+        _interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        /*GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        //Grab the object
+        if (_interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(_interactable);
+            hand.HideGrabHint();
+        }
+
+        //Release the object
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(_interactable);
+        }*/
+    }
+}
